e973b654d0d26407a85654f000bf39c69fe28a27
ingridmanfrin <ingrid.caldeira@ITLNB109>
2023-04-26 02:55:21 -0300

    Adicionar arquivos de projeto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace Foreach_em_CSharp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ListBox = new System.Windows.Forms.ListBox();
+            this.BtnOK = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ListBox
+            // 
+            this.ListBox.FormattingEnabled = true;
+            this.ListBox.Location = new System.Drawing.Point(88, 42);
+            this.ListBox.Name = "ListBox";
+            this.ListBox.Size = new System.Drawing.Size(216, 199);
+            this.ListBox.TabIndex = 0;
+            // 
+            // BtnOK
+            // 
+            this.BtnOK.Location = new System.Drawing.Point(152, 260);
+            this.BtnOK.Name = "BtnOK";
+            this.BtnOK.Size = new System.Drawing.Size(75, 23);
+            this.BtnOK.TabIndex = 1;
+            this.BtnOK.Text = "OK";
+            this.BtnOK.UseVisualStyleBackColor = true;
+            this.BtnOK.Click += new System.EventHandler(this.BtnOK_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(429, 327);
+            this.Controls.Add(this.BtnOK);
+            this.Controls.Add(this.ListBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox ListBox;
+        private System.Windows.Forms.Button BtnOK;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Foreach_em_CSharp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnOK_Click(object sender, EventArgs e)
+        {
+            //for each
+
+            List<string> lista_nomes = new List<string>()
+            {
+                "leo", "jose", "alex", "maria", "joao", "marcia"
+            };
+
+            foreach (string ls in lista_nomes) // para cada string que estás contida em lista_nomes vai ser "jogado dentro" de ls
+            {
+                ListBox.Items.Add(ls);
+            }
+
+            //string frase = "Meu curso de C#.";
+            //ListBox.Items.Add(frase.Lenght); //comprimento
+
+            //foreach (char f in frase) // f é o apelido que dei para cada caracter da variável frase
+            //{
+            //    ListBox.Items.Add(f);   
+            //}
+        }
+    }
+}
